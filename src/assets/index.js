edd71ca5c6ca2e2481946a4a16089dfd57f7cb53
Mikwuegbu <mstephen130@yahoo.com>
2024-04-19 23:26:11 +0100

    ui finished

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,7 @@ import checkmark from './check-mark.png';
 import pen from './pen.png';
 import recycleBin from './recycle-bin.png';
 import arrow from './arrow.png';
+import list from './list.png';
+import tick from './tick.png';
 
-export { calendar, checkmark, pen, recycleBin, arrow };
+export { calendar, checkmark, pen, recycleBin, arrow, list, tick };
